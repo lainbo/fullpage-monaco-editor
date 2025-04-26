b8754dc62316157643b4a7705e1a55c38d8f5d22
Jason Liang <lainbo@foxmail.com>
2025-04-26 16:00:28 +0800

    Add vite-plugin-compression plugin to support Gzip compression, update package.json and vite.config.ts files to configure compression options.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
+import compression from 'vite-plugin-compression';
 
 export default defineConfig({
   build: {
     target: 'esnext',
-  }
+  },
+  plugins: [
+    compression({
+      algorithm: 'gzip', // 使用gzip压缩
+      ext: '.gz', // 生成的文件后缀
+      threshold: 10241, // 体积大于10KB的文件才会被压缩
+    })
+  ]
 }); 
